9b290936a514860122d3626d0c3e40a3c9f4cf28
edwinrjrc <edwinrjrc@gmail.com>
2021-05-01 00:20:40 -0500

    Ingredientes por plato

---
+++ mf-jpa-ingrediente/src/test/java/pe/com/rhsistemas/mfjpaingrediente/MfJpaIngredienteApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.rhsistemas.mfjpaingrediente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfJpaIngredienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
